89eb77f57847684c73dbe73fb836f238a9e41671
ryouze <98982999+ryouze@users.noreply.github.com>
2022-11-05 17:00:47 +0100

    use pointers in heron

--- c/heron.c
+++ c/heron.c
@@ -2,15 +2,15 @@
 #include <math.h>
 #include <stdio.h>
 
-float heron(int x, int max_iter, int max_decimals) {
-    float g = x / 2;  // initial guess
+float heron(int *x, int *max_iter, int *max_decimals) {
+    float g = *x / 2;  // initial guess
     // rounding hack: 1 decimal = 10, 2 decimals = 100, 3 decimals = 1,000
-    int round_hack = pow(10, max_decimals);
-    for (int i = 1; i < max_iter + 1; i++) {
-        g = (g + x / g) / 2;
+    int round_hack = pow(10, *max_decimals);
+    for (int i = 1; i < *max_iter + 1; i++) {
+        g = (g + *x / g) / 2;
         g = round(g * round_hack) / round_hack;
         printf("iter_no=%i; g=%g\n", i, g);  // %g skips trailing zeros
-        if (pow(g, 2) == x)
+        if (pow(g, 2) == *x)
             return g;
     }
     return g;
@@ -18,6 +18,6 @@ float heron(int x, int max_iter, int max_decimals) {
 
 int main(void) {
     int x = 25, max_iter = 10, max_decimals = 3;
-    printf("The square root of %i is %g.\n", x, heron(x, max_iter, max_decimals));
+    printf("The square root of %i is %g.\n", x, heron(&x, &max_iter, &max_decimals));
     return 0;
 }
