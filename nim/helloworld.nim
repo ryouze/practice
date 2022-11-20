00ae66975d1eb95b279342c3d98f2d4def249810
ryouze <98982999+ryouze@users.noreply.github.com>
2022-11-20 03:39:39 +0100

    update nim files

--- nim/helloworld.nim
+++ nim/helloworld.nim
@@ -1,11 +1,23 @@
+#[
+    Nim (formerly Nimrod) is a statically typed, imperative programming language.
+]#
+
+# print string to console
 echo "Hello World"
 
-# To compile this, run the following:
-# nim c -r --verbosity:0 helloworld.nim
+#[
 
-# The command above has several elements:
-# c is an alias for compile, which compiles the Nim sources into C and then invokes the C compiler on it.
-# -r is an alias for --run, which runs the program.
-# --verbosity:0 makes the compiler only output essential messages, since by default it also outputs some debugging messages. From now on, we assume that --verbosity:0 is set.
-# ./helloworld.nim is the path to the source you want to compile.
+    To compile and run this, enter the following:
+    nim c --verbosity:0 -o=bin/a -r helloworld.nim
 
+    The command above has several elements:
+
+        c is an alias for compile, which compiles the Nim sources into C, and then invokes the C compiler on it.
+
+        -r is an alias for --run, which runs the program.
+
+        --verbosity:0 makes the compiler only output essential messages, since by default it also outputs some debugging messages. From now on, we assume that --verbosity:0 is enabled.
+
+        ./helloworld.nim is the path to the source you want to compile.
+
+]#
